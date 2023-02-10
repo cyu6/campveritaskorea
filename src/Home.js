529aa0192b7910de2e0a705c7a79e13e9897adf8
Catherine Yu <cyu@stripe.com>
2023-02-10 01:32:43 -0800

    super basic navbar

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './Home.css';
+import NavBar from './NavBar';
+
+function Home() {
+  return (
+    <div className="Home">
+      <NavBar />
+    </div>
+  );
+}
+
+export default Home;
